d4639a5fc6ffa86a5d62e0b7c0b836007350e213
PontshoMotsikwe <preevision@gmail.com>
2025-08-29 03:37:33 +0200

    week3 python assignment

---
+++ week3.py
@@ -0,0 +1,14 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent / 100)
+        return price - discount_amount
+    else:
+        return price
+
+
+price = float(input("Enter the original price of the item: "))
+discount_percent = float(input("Enter the discount percentage: "))
+
+final_price = calculate_discount(price, discount_percent)
+
+print("Final Price:", final_price)
